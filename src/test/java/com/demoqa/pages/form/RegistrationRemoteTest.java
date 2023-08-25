9a5cec1444aeb1a1f073448b9c1798651c2d6441
Evgenyi <shevchuk291992@gmail.com>
2023-08-26 00:15:08 +0300

    jenkins_remote

---
+++ src/test/java/com/demoqa/pages/form/RegistrationRemoteTest.java
@@ -0,0 +1,49 @@
+package com.demoqa.pages.form;
+
+import com.demoqa.pages.pages.RegistrationPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.demoqa.pages.utils.TestDataParam.*;
+
+public class RegistrationRemoteTest extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    @Tag("remote")
+    @DisplayName("Тестирование формы регистрации")
+    void fillFormTest() {
+        registrationPage
+                .openPage()
+                .settingsRegistrationPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setUserNumber(phoneNumber)
+                .setBirthDate(day, month, year)
+                .setUserHobbies(hobby)
+                .setUploadPicture(fileName)
+                .setCurrentAddress(address)
+                .setSubjectsInput(subject)
+                .choiceState(state)
+                .choiceCity(city)
+                .clickSubmit()
+
+                .thxForm("Thanks for submitting the form");
+
+        registrationPage
+                .checkResult(firstName + " " + lastName)
+                .checkResult(email)
+                .checkResult(gender)
+                .checkResult(phoneNumber)
+                .checkResult(day + " " + month + "," + year)
+                .checkResult(subject)
+                .checkResult(fileName)
+                .checkResult(address)
+                .checkResult(state + " " + city)
+                .closeModal();
+    }
+}
